089ee8c0b19349e36662937af19728b3fa852518
joskalenda <joskalenda3@gmail.com>
2022-03-23 15:12:49 +0200

    Update queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals;
+select * from animals WHERE name like '%mon';
+select * from animals WHERE date_of_birth Between '2016-01-01' AND '2019-12-31';
+select * from animals WHERE neutered = true AND  escape_attempts < 3;
+select * from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+select name, escape_attempts from animals WHERE weight_kg > 10.5;
+select name from animals WHERE neutered = true;
+select name from animals WHERE name <> 'Gabumon';
